de4d6ebb4463ccb68ba375148209d69ce6145f8d
dodzi2022 <99569396+dodzi2022@users.noreply.github.com>
2022-04-24 04:12:35 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
+terraform {
+
+    backend "s3" {
+       bucket= "dsg-bucket1"
+       key= "dsg-bucket1/dev"
+       region = "us-east-1"
+
+       }
+} 
+
 provider "aws" {
 
     region= var.region
